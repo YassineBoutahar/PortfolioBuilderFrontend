b88a9f9759106ada51ee4cd1d1f619830a608d14
Yassine Boutahar <yassineboutahar@cmail.carleton.ca>
2021-02-22 23:20:11 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,418 @@
+import React, { useState, useEffect } from "react";
+import HoldingList from "./Components/HoldingList";
+import Charts from "./Components/Charts";
+import SiteLogo from "./Components/SiteLogo";
+import {
+  TextField,
+  IconButton,
+  ButtonGroup,
+  InputAdornment,
+  Box,
+  Typography,
+  Snackbar,
+  makeStyles,
+} from "@material-ui/core";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import ShareIcon from "@material-ui/icons/Share";
+import { Holding, LocalStorageItem, AppProps } from "./types";
+import randomColor from "randomcolor";
+import moment from "moment";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+const holdingsKey = "PortfolioBuilderHoldings";
+const backendUrl = "http://portfoliobuilder.us-east-2.elasticbeanstalk.com/";
+
+const useStyles = makeStyles({
+  body: {
+    marginTop: "0.25rem",
+    height: "100%",
+  },
+  portfolioSection: {
+    marginRight: "0.75rem",
+  },
+  portfolioBar: {
+    marginBottom: "1rem",
+  },
+  chartSection: {
+    marginLeft: "0.75rem",
+  },
+  topBarActionButton: {
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  buttonGroupOverline: {
+    lineHeight: 1.75,
+    fontSize: "0.7rem",
+  },
+});
+
+const App = ({ urlShareHash }: AppProps) => {
+  const classes = useStyles();
+  function Alert(props: AlertProps) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const [totalValue, setTotalValue] = useState<number>(0);
+  const [holdings, setHoldings] = useState<Map<string, Holding>>(new Map());
+  const [remainingPercent, setRemainingPercent] = useState<number>(100);
+  const [chosenTimePeriod, setTimePeriod] = useState<"y" | "M" | "w">("y");
+  const [chosenInterval, setPriceInterval] = useState<"1mo" | "1d" | "1wk">(
+    "1wk"
+  );
+  const [tickerSearch, setTickerSearch] = useState<string>("");
+  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (urlShareHash) {
+      fetchFromShareLink(urlShareHash);
+    } else {
+      if (window.localStorage.getItem(holdingsKey)) {
+        let existingHoldings: LocalStorageItem[] = JSON.parse(
+          window.localStorage.getItem(holdingsKey) || "[]"
+        ) as LocalStorageItem[];
+        existingHoldings.map((h) =>
+          addQuote(h.ticker, h.portfolioPercentage, true)
+        );
+      }
+    }
+  }, []);
+
+  const fetchFromShareLink = (shareHash: string) => {
+    axios
+      .get(`${backendUrl}/share/${shareHash}`)
+      .then((response) => {
+        let sharedHoldings = JSON.parse(
+          response.data.PortfolioObj.S
+        ) as LocalStorageItem[];
+        sharedHoldings.map((h) =>
+          addQuote(h.ticker, h.portfolioPercentage, true)
+        );
+      })
+      .catch((err) => alert(err));
+  };
+
+  const insertHolding = (
+    ticker: string,
+    newHolding: Holding,
+    upsert: boolean = false
+  ) => {
+    if (!upsert && holdings.get(ticker)) {
+      alert("You have already added this ticker.");
+      return false;
+    } else {
+      setHoldings((prev) => new Map(prev).set(ticker, newHolding));
+      let localStorageHoldings = Array.from(holdings.values())
+        .filter((holding) => holding.ticker !== ticker)
+        .map((h) => ({
+          ticker: h.ticker,
+          portfolioPercentage: h.portfolioPercentage,
+        }));
+      window.localStorage.setItem(
+        holdingsKey,
+        JSON.stringify([
+          ...localStorageHoldings,
+          {
+            ticker: ticker,
+            portfolioPercentage: newHolding.portfolioPercentage,
+          },
+        ])
+      );
+      return true;
+    }
+  };
+
+  const deleteHolding = (ticker: string) => {
+    let localStorageHoldings = Array.from(holdings.values())
+      .filter((holding) => holding.ticker !== ticker)
+      .map((h) => ({
+        ticker: h.ticker,
+        portfolioPercentage: h.portfolioPercentage,
+      }));
+    window.localStorage.setItem(
+      holdingsKey,
+      JSON.stringify(localStorageHoldings)
+    );
+    setHoldings((prev) => {
+      const ogState = new Map(prev);
+      ogState.delete(ticker);
+      return ogState;
+    });
+  };
+
+  const updateAllQuotes = () => {
+    Array.from(holdings.keys()).forEach((ticker) => updateQuote(ticker));
+  };
+
+  const addQuote = (
+    ticker: string,
+    portfolioPercentage: number = 0,
+    forRefresh: boolean = false
+  ) => {
+    axios
+      .get(`${backendUrl}/quote/${ticker}`)
+      .then((response) => {
+        if (!response.data) {
+          throw Object.assign(new Error("Ticker not found!"), { code: 404 });
+        }
+        let newHolding: Holding = {
+          ticker: ticker,
+          name: response.data.longName,
+          currency: response.data.financialCurrency,
+          exchange: response.data.fullExchangeName,
+          currentPrice: parseFloat(response.data.regularMarketPrice),
+          previousClosePrice: parseFloat(
+            response.data.regularMarketPreviousClose ||
+              response.data.regularMarketOpen
+          ),
+          portfolioPercentage: portfolioPercentage,
+          displayColor: randomColor({ luminosity: "bright" }),
+        };
+        if (insertHolding(ticker, newHolding, forRefresh)) {
+          setTickerSearch("");
+          getHistoricalData(
+            newHolding,
+            moment().subtract(1, chosenTimePeriod),
+            chosenInterval
+          );
+        }
+      })
+      .catch((error) => {
+        alert("Ticker not found!");
+      });
+  };
+
+  const updateQuote = (ticker: string) => {
+    axios
+      .get(`${backendUrl}/quote/${ticker}`)
+      .then((response) => {
+        let ogHolding = holdings.get(ticker);
+        if (ogHolding) {
+          ogHolding.currentPrice = parseFloat(response.data.regularMarketPrice);
+          ogHolding.previousClosePrice = parseFloat(
+            response.data.regularMarketPreviousClose ||
+              response.data.regularMarketOpen
+          );
+          insertHolding(ticker, ogHolding, true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const refreshAllHistoricalData = (
+    startDate: moment.Moment,
+    interval: "1d" | "1wk" | "1mo" | undefined,
+    deletedTicker?: string
+  ) => {
+    Array.from(holdings.values()).map((holding) => {
+      if (holding.ticker !== deletedTicker) {
+        getHistoricalData(holding, startDate, interval, true);
+      }
+      return null;
+    });
+  };
+
+  const getHistoricalData = (
+    holding: Holding,
+    startDate: moment.Moment,
+    interval: "1d" | "1wk" | "1mo" | undefined,
+    refreshing: boolean = false
+  ) => {
+    axios
+      .get(`${backendUrl}/historical/${holding.ticker}`, {
+        params: {
+          period1: startDate.format("YYYY-MM-DD"),
+          interval: interval,
+        },
+      })
+      .then((response) => {
+        holding.historicalData = response.data;
+        insertHolding(holding.ticker, holding, true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const updatePortfolioPercentage = (ticker: string, percent: number) => {
+    let ogHolding = holdings.get(ticker);
+    if (ogHolding) {
+      // setRemainingPercent(
+      //   (prev) => prev - (percent + (ogHolding?.portfolioPercentage || 0))
+      // );
+      ogHolding.portfolioPercentage = percent;
+      insertHolding(ticker, ogHolding, true);
+    }
+  };
+
+  const getAvailablePercentage = (ticker: string) => {
+    return Math.max(
+      0,
+      100 -
+        Array.from(holdings.values())
+          .map((a) => a.portfolioPercentage)
+          .reduce((acc, p) => acc + p, 0) +
+        (holdings.get(ticker)?.portfolioPercentage || 0)
+    );
+  };
+
+  const getEstimatedShares = (ticker: string) => {
+    let holdingObj = holdings.get(ticker);
+    return Math.floor(
+      (totalValue * (holdingObj?.portfolioPercentage || 0)) /
+        100 /
+        (holdingObj?.currentPrice || 99999999)
+    );
+  };
+
+  const createShareLink = () => {
+    let newUUID = uuidv4();
+    axios
+      .post(
+        `${backendUrl}/share`,
+        {
+          portfolio: Array.from(holdings.values()).map((h) => ({
+            ticker: h.ticker,
+            portfolioPercentage: h.portfolioPercentage,
+          })),
+        },
+        { params: { shareHash: newUUID } }
+      )
+      .then((response) => {
+        let shareUrl = `${window.location.host}/share/${newUUID}`;
+        navigator.clipboard
+          .writeText(shareUrl)
+          .then(() => setSnackbarOpen(true));
+      })
+      .catch((err) => alert(`Could not fetch portfolio from DynamoDB. ${err}`));
+  };
+
+  return (
+    <Box
+      className={classes.body}
+      display="flex"
+      flexDirection="row"
+      flex={1}
+      width={1}
+    >
+      <Box width={1} className={classes.portfolioSection}>
+        <Box display="flex" flexDirection="column" width={1}>
+          <Box textAlign="center">
+            <Typography
+              className={classes.buttonGroupOverline}
+              variant="overline"
+              color="textSecondary"
+              align="center"
+            >
+              Portfolio Controls
+            </Typography>
+          </Box>
+          <Box>
+            <Box
+              className={classes.portfolioBar}
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+              flex={1}
+              width={1}
+            >
+              <Box>
+                <SiteLogo />
+              </Box>
+              <Box flexDirection="row">
+                <TextField
+                  id="standard-number"
+                  label="Add a stock"
+                  placeholder="Ticker"
+                  value={tickerSearch}
+                  onChange={(e) =>
+                    setTickerSearch(e.target.value.toUpperCase())
+                  }
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      addQuote(tickerSearch);
+                      e.preventDefault();
+                    }
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    ),
+                  }}
+                />
+                <IconButton onClick={() => addQuote(tickerSearch)} edge="start">
+                  <AddCircleOutlineIcon />
+                </IconButton>
+              </Box>
+              <TextField
+                error={isNaN(totalValue) || totalValue < 0}
+                label="Total Portfolio Value"
+                type="number"
+                value={totalValue}
+                onChange={(e) => setTotalValue(parseFloat(e.target.value))}
+                InputProps={{
+                  inputProps: { min: 0 },
+                  startAdornment: (
+                    <InputAdornment position="start">$</InputAdornment>
+                  ),
+                }}
+                helperText={
+                  isNaN(totalValue) || totalValue < 0 ? "Invalid Value" : ""
+                }
+              />
+              <ButtonGroup aria-label="top bar actions">
+                <IconButton
+                  className={classes.topBarActionButton}
+                  onClick={() => createShareLink()}
+                >
+                  <ShareIcon />
+                </IconButton>
+                <IconButton
+                  className={classes.topBarActionButton}
+                  onClick={() => updateAllQuotes()}
+                >
+                  <RefreshIcon />
+                </IconButton>
+              </ButtonGroup>
+            </Box>
+          </Box>
+        </Box>
+        <Box>
+          <HoldingList
+            holdings={Array.from(holdings.values())}
+            portfolioValue={totalValue}
+            insertHolding={insertHolding}
+            deleteHolding={deleteHolding}
+            updatePortfolioPercentage={updatePortfolioPercentage}
+            getAvailablePercentage={getAvailablePercentage}
+          />
+        </Box>
+      </Box>
+      <Box className={classes.chartSection} width={1}>
+        <Charts
+          holdings={holdings}
+          timePeriod={chosenTimePeriod}
+          setTimePeriod={setTimePeriod}
+          interval={chosenInterval}
+          setPriceInterval={setPriceInterval}
+          refreshAllHistoricalData={refreshAllHistoricalData}
+        />
+      </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={4000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert onClose={() => setSnackbarOpen(false)} severity="success">
+          Share link copied to clipboard!
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/Components/Charts.tsx
@@ -0,0 +1,382 @@
+import React, { useState } from "react";
+import { Box, Typography, makeStyles, useTheme } from "@material-ui/core";
+import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab";
+import { Polar, Line } from "react-chartjs-2";
+
+import PieChartIcon from "@material-ui/icons/PieChart";
+import TimelineIcon from "@material-ui/icons/Timeline";
+import { ChartsProps, Holding } from "../types";
+import { ChartDataSets, ChartPoint } from "chart.js";
+import moment from "moment";
+import { HistoricalResult } from "yahoo-finance2/api/modules/historical";
+
+const useStyles = makeStyles({
+  buttonGroupOverline: {
+    lineHeight: 1.75,
+    fontSize: "0.7rem",
+  },
+  lineChartButtonGroup: {
+    marginRight: "0.5rem",
+  },
+  chartTitle: {
+    marginRight: "0.5rem",
+    marginLeft: "0.5rem",
+    marginTop: "1rem",
+  },
+});
+
+const Charts = ({
+  holdings,
+  timePeriod,
+  setTimePeriod,
+  interval,
+  setPriceInterval,
+  refreshAllHistoricalData,
+}: ChartsProps) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [chartType, setChartType] = useState("polar");
+
+  const handleChartChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newChartType: string | null
+  ) => {
+    if (newChartType !== null) {
+      setChartType(newChartType);
+    }
+  };
+
+  const handlePeriodChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newPeriod: "y" | "M" | "w" | null
+  ) => {
+    if (newPeriod !== null) {
+      setTimePeriod(newPeriod);
+      refreshAllHistoricalData(moment().subtract(1, newPeriod), interval);
+    }
+  };
+
+  const handleIntervalChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newInterval: "1mo" | "1d" | "1wk" | null
+  ) => {
+    if (newInterval !== null) {
+      setPriceInterval(newInterval);
+      refreshAllHistoricalData(moment().subtract(1, timePeriod), newInterval);
+    }
+  };
+
+  const getChartDataSets = () => {
+    let allChartDataSets: ChartDataSets[] = Array.from(holdings.values())
+      .filter((holding) => holding.historicalData !== undefined)
+      .map((holding) => ({
+        label: holding.ticker,
+        data: parseHistoricalDate(
+          holding.historicalData as HistoricalResult
+        )[1],
+        fill: false,
+        borderColor: holding.displayColor,
+      }));
+
+    allChartDataSets.push(getWeightedAverage(allChartDataSets));
+    return allChartDataSets;
+  };
+
+  const getChartLabel = () => {
+    let allLabelArrays = Array.from(holdings.values())
+      .filter((holding) => holding.historicalData !== undefined)
+      .map(
+        (holding) =>
+          parseHistoricalDate(holding.historicalData as HistoricalResult)[0]
+      );
+    let largestLabelArray = allLabelArrays
+      .map((h) => h.length)
+      .indexOf(Math.max(...allLabelArrays.map((h) => h.length)));
+
+    return allLabelArrays[largestLabelArray];
+  };
+
+  const parseHistoricalDate = (dataset: HistoricalResult) => {
+    //console.log(dataset.map((point) => ({t: point.date, y: point.close})));
+    let chartLabels: string[] = dataset.map((point) =>
+      moment(point.date).format("YYYY/MM/DD")
+    );
+    let chartData: ChartPoint[] = dataset.map((point) => ({
+      t: point.date,
+      y: Math.round((point.close + Number.EPSILON) * 100) / 100,
+    }));
+
+    let resObj: [string[], ChartPoint[]] = [chartLabels, chartData];
+    return resObj;
+  };
+
+  const getWeightedAverage = (allDataSets: ChartDataSets[]) => {
+    return {
+      label: "Weighted Avg",
+      data: generatedWeightedAveragePoints(allDataSets),
+      fill: false,
+      borderColor: theme.palette.text.secondary,
+      backgroundColor: theme.palette.text.secondary,
+      borderDash: [5, 5],
+      pointBackgroundColor: theme.palette.text.secondary,
+      pointBorderColor: theme.palette.text.primary,
+    };
+  };
+
+  const generatedWeightedAveragePoints = (allDataSets: ChartDataSets[]) => {
+    let chartPointsMap: Map<string, number> = new Map();
+    allDataSets.forEach((holdingDataSet) => {
+      let holding: Holding | undefined = holdings.get(
+        holdingDataSet.label || ""
+      );
+      if (!holding) {
+        return;
+      }
+      let portfolioPercentage = holding.portfolioPercentage;
+      let currentChartPoints: ChartPoint[] =
+        (holdingDataSet.data as ChartPoint[]) || [];
+      currentChartPoints.forEach((point: ChartPoint) => {
+        chartPointsMap.set(
+          point.t as string,
+          (chartPointsMap.get(point.t as string) || 0) +
+            ((point.y as number) * portfolioPercentage) / 100
+        );
+      });
+    });
+
+    return Array.from(chartPointsMap).map(([time, price]) => ({
+      t: time,
+      y: Math.round((price + Number.EPSILON) * 100) / 100,
+    }));
+  };
+
+  return (
+    <Box>
+      <Box
+        width={1}
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box display="flex" flexDirection="column">
+          <Typography
+            className={classes.buttonGroupOverline}
+            variant="overline"
+            color="textSecondary"
+          >
+            Chart Type
+          </Typography>
+          <ToggleButtonGroup
+            value={chartType}
+            exclusive
+            onChange={handleChartChange}
+            hidden={false}
+            aria-label="Chart style"
+          >
+            <ToggleButton value="polar" aria-label="polar area">
+              <PieChartIcon fontSize="small" />
+            </ToggleButton>
+            <ToggleButton value="line" aria-label="line chart">
+              <TimelineIcon fontSize="small" />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+
+        <Box display="flex" alignSelf="center">
+          <Typography
+            className={classes.chartTitle}
+            align="center"
+            variant="h6"
+            color="textPrimary"
+          >
+            {chartType === "line"
+              ? "Historical Portfolio Performance"
+              : "Portfolio Breakdown"}
+          </Typography>
+        </Box>
+
+        <Box display="flex" style={{ minWidth: "5.5rem" }}>
+          {chartType === "line" ? (
+            <Box display="flex" flexDirection="row">
+              <Box
+                display="flex"
+                flexDirection="column"
+                className={classes.lineChartButtonGroup}
+              >
+                <Typography
+                  className={classes.buttonGroupOverline}
+                  variant="overline"
+                  color="textSecondary"
+                >
+                  Time period
+                </Typography>
+                <ToggleButtonGroup
+                  value={timePeriod}
+                  size="small"
+                  exclusive
+                  onChange={handlePeriodChange}
+                  aria-label="Start date"
+                >
+                  <ToggleButton
+                    value="w"
+                    aria-label="last week"
+                    disabled={interval === "1mo"}
+                  >
+                    <Typography>
+                      <b>1W</b>
+                    </Typography>
+                  </ToggleButton>
+                  <ToggleButton value="M" aria-label="last month">
+                    <Typography>
+                      <b>1M</b>
+                    </Typography>
+                  </ToggleButton>
+                  <ToggleButton value="y" aria-label="last year">
+                    <Typography>
+                      <b>1Y</b>
+                    </Typography>
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </Box>
+
+              <Box
+                display="flex"
+                flexDirection="column"
+                className={classes.lineChartButtonGroup}
+              >
+                <Typography
+                  className={classes.buttonGroupOverline}
+                  variant="overline"
+                  color="textSecondary"
+                >
+                  Price interval
+                </Typography>
+                <ToggleButtonGroup
+                  value={interval}
+                  size="small"
+                  exclusive
+                  onChange={handleIntervalChange}
+                  aria-label="Point interval"
+                >
+                  <ToggleButton value="1d" aria-label="per day">
+                    <Typography>
+                      <b>1D</b>
+                    </Typography>
+                  </ToggleButton>
+                  <ToggleButton value="1wk" aria-label="per week">
+                    <Typography>
+                      <b>1W</b>
+                    </Typography>
+                  </ToggleButton>
+                  <ToggleButton
+                    value="1mo"
+                    aria-label="per month"
+                    disabled={timePeriod === "w"}
+                  >
+                    <Typography>
+                      <b>1M</b>
+                    </Typography>
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </Box>
+            </Box>
+          ) : (
+            <></>
+          )}
+        </Box>
+      </Box>
+
+      <Box>
+        {chartType === "polar" ? (
+          <Polar
+            data={{
+              datasets: [
+                {
+                  data: Array.from(holdings.values()).map(
+                    (h) => h.portfolioPercentage
+                  ),
+                  backgroundColor: Array.from(holdings.values()).map(
+                    (h) => h.displayColor
+                  ),
+                },
+              ],
+
+              // These labels appear in the legend and in the tooltips when hovering different arcs
+              labels: Array.from(holdings.values()).map((h) => h.ticker),
+            }}
+            options={{
+              title: {
+                display: false,
+                text: "Portfolio Breakdown",
+              },
+              legend: {
+                labels: {
+                  fontColor: theme.palette.text.primary,
+                },
+              },
+              elements: {
+                arc: {
+                  borderColor: theme.palette.text.secondary,
+                },
+              },
+              scale: {
+                gridLines: {
+                  color: theme.palette.text.secondary,
+                },
+                pointLabels: {
+                  fontColor: theme.palette.text.secondary,
+                  // Include a dollar sign in the ticks
+                  callback: function (value) {
+                    return value + "%";
+                  },
+                },
+              },
+            }}
+          />
+        ) : (
+          <Line
+            data={{
+              labels: getChartLabel(),
+              datasets: getChartDataSets(),
+            }}
+            options={{
+              title: {
+                display: false,
+                text: "Historical Portfolio Performance",
+              },
+              legend: {
+                labels: {
+                  fontColor: theme.palette.text.primary,
+                },
+              },
+              scales: {
+                yAxes: [
+                  {
+                    ticks: {
+                      fontColor: theme.palette.text.secondary,
+                      // Include a dollar sign in the ticks
+                      callback: function (value, index, values) {
+                        return "$" + value;
+                      },
+                    },
+                  },
+                ],
+                xAxes: [
+                  {
+                    ticks: {
+                      fontColor: theme.palette.text.secondary,
+                    },
+                  },
+                ],
+              },
+            }}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Charts;
